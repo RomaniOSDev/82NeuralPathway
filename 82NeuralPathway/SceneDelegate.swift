5c0e64e80aad1bdcc6ff0661da56e24f3afbc59d
Roman <romazan.26@gmail.com>
2026-03-27 09:15:02 +0300

    add view

--- 82NeuralPathway/SceneDelegate.swift
+++ 82NeuralPathway/SceneDelegate.swift
@@ -12,12 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private var appRouter: AppRouter?
+
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else {return}
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: ContentView())
+        appRouter = AppRouter()
+        window?.rootViewController = appRouter?.initialViewController()
         window?.makeKeyAndVisible()
+    
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ 82NeuralPathway/Views/Router/SaveService.swift
@@ -0,0 +1,15 @@
+//
+//  SaveService.swift
+//  82NeuralPathway
+//
+
+
+import Foundation
+
+struct SaveService {
+    
+    static var lastUrl: URL? {
+        get { UserDefaults.standard.url(forKey: "LastUrl") }
+        set { UserDefaults.standard.set(newValue, forKey: "LastUrl") }
+    }
+}
